be6d82b9e738ff331dca5f3b5c7f9774c2b42514
Mehrubon <nabievm811@gmail.com>
2025-09-20 15:31:46 +0300

    0

--- extensions.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_mail import Mail
-from flask_login import LoginManager
-
-db = SQLAlchemy()
-mail = Mail()
-login_manager = LoginManager()
-login_manager.login_view = 'login'
-login_manager.login_message_category = 'info'
